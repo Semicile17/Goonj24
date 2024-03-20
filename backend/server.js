4de16817869e379fb82ced96a7e9a8ce4f030636
vky5 <vky05@proton.me>
2024-03-20 23:23:20 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+const app = require('./main');
+
+dotenv.config({path: './config.env'});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
+})
