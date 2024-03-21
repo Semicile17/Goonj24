327a318bb676696fe294ba01af6692a52922558c
vky5 <vky05@proton.me>
2024-03-21 05:39:06 +0530

    resolved cors issue

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
+
 
 const authRoute = require('./routes/authRoute')
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PATCH,DELETE'
+}))
 
 app.use(morgan('dev'));
 
